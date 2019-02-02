02287242d1c8a4f89a5cf31557cc6f4e7de77486
Alexander L. Edwards <ALEdwards21@chaminet.org>
2019-02-02 10:42:15 -0800

    Auton driveForward with encoders command

---
+++ src/main/java/frc/commands/DriveForward.java
@@ -0,0 +1,33 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.subsystems.DriveSubsystem;
+
+public class DriveForward extends Command {
+	public DriveForward() {
+		requires(Robot.DRIVE_SUBSYSTEM);
+	}
+
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void execute() {
+		Robot.DRIVE_SUBSYSTEM.moveForward(DriveSubsystem.KDrivePosition);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+
+	@Override
+	protected void end() {
+	}
+
+	@Override
+	protected void interrupted() {
+	}
+}
