ee9c9e18a6f69903f13f2c36872e7cb5237469dd
Eagle Engineering <eeuser@chaminet.net>
2019-02-17 07:52:02 -0800

    forgot to reset the variable

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -117,6 +117,7 @@ public class Robot extends TimedRobot {
     LIFT_SUBSYSTEM.resetLiftEncoder();
     ARM_SUBSYSTEM.resetLeftArmEncoder();
     ARM_SUBSYSTEM.resetRightArmEncoder();
+    pdp.voltageSpikeOccured = false;
     SmartDashboard.putString("Robot Encoders", "NOT ALIGNED");
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
