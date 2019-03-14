f00eccf06352651e7e01681fd0c79696b42324b1
Eagle Engineering <eeuser@chaminet.net>
2019-03-14 08:06:13 -0700

    PostMortem Collecting command now manages its subcommands

--- src/main/java/frc/commands/ScoringCommandGroups/CollectingPositionPostMortem.java
+++ src/main/java/frc/commands/ScoringCommandGroups/CollectingPositionPostMortem.java
@@ -7,7 +7,7 @@
 
 package frc.commands.ScoringCommandGroups;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.commands.Arm.MoveArmToPosition;
 import frc.commands.Lift.MoveLiftToPosition;
@@ -15,17 +15,68 @@ import frc.robot.Robot;
 import frc.subsystems.ArmSubsystem.ArmPosition;
 import frc.subsystems.LiftSubsystem.LiftPosition;
 
-public class CollectingPositionPostMortem extends CommandGroup {
-  /**
-   * Add your docs here.
-   */
+public class CollectingPositionPostMortem extends Command {
+  
+  MoveArmToPosition moveArmToMiddle;
+  MoveLiftToPosition moveLiftToCargoPos;
+  MoveArmToPosition moveArmFullUp;
+
+  int stage = 0;
+  boolean canRun = false;
+
   public CollectingPositionPostMortem() {
-    if(Robot.CARRIAGE_SUBSYSTEM.BumpSwitch.get())
-    {
-      addSequential(new MoveArmToPosition(ArmPosition.MIDDLE));
-      addSequential(new MoveLiftToPosition(LiftPosition.CARGO));
-      addSequential(new MoveArmToPosition(ArmPosition.FULLUP));
-      SmartDashboard.putNumber("amount cycled entered", 1);
+    moveArmToMiddle = new MoveArmToPosition(ArmPosition.MIDDLE);
+    moveLiftToCargoPos = new MoveLiftToPosition(LiftPosition.CARGO);
+    moveArmFullUp = new MoveArmToPosition(ArmPosition.FULLUP);
+  }
+
+  @Override
+  protected void initialize() {
+    stage = 0;
+    canRun = Robot.CARRIAGE_SUBSYSTEM.BumpSwitch.get();
+    SmartDashboard.putNumber("postmortem stage", stage);
+  }
+
+  @Override
+  protected void execute() {
+    if (!canRun) return;
+    switch (stage) {
+      case 0:
+        moveArmToMiddle.start();
+        stage++;
+        break;
+      case 1:
+        if (!moveArmToMiddle.isRunning()) {
+          moveLiftToCargoPos.start();
+          stage++;
+        }
+        break;
+      case 2:
+        if (!moveLiftToCargoPos.isRunning()) {
+          moveArmFullUp.start();
+          stage++;
+        }
+        break;
+      case 3:
+        if (!moveArmFullUp.isRunning()) {
+          stage++; // Once stage is 4, and not here, then we're done
+        }
+        break;
     }
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return !canRun || (stage == 4); // If we can't run, we're finished
+  }
+
+  @Override
+  protected void end() {
+
+  }
+
+  @Override
+  protected void interrupted() {
+
   }
 }
